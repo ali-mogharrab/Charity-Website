cd8e11f1ac72e1050d35fa65fc0552a6b67a4e36
Ali Mogharrab <alimogharrab101@gmail.com>
2023-01-20 23:53:10 +0330

    Add accounts models and charities models

--- charities/models.py
+++ charities/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Benefactor(models.Model):
+    EXP_CHOICES = (
+        (0, 'Begginer'),
+        (1, 'Intermediate'),
+        (2, 'Advanced'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    experience = models.SmallIntegerField(choices=EXP_CHOICES, default=0)
+    free_time_per_week = models.PositiveSmallIntegerField(default=0)
+
+class Charity(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=10)
+
+class Task(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    STATE_CHOICES = (
+        ('P', 'Pending'),
+        ('W', 'Waiting'),
+        ('A', 'Assigned'),
+        ('D', 'Done'),
+    )
+    assigned_benefactor = models.ForeignKey(Benefactor, on_delete=models.SET_NULL, null=True)
+    charity = models.ForeignKey(Charity, on_delete=models.SET_NULL, null=True)
+    age_limit_from = models.IntegerField(null=True, blank=True)
+    age_limit_to = models.IntegerField(null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    gender_limit = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    state = models.CharField(max_length=1, choices=STATE_CHOICES, default='P')
+    title = models.CharField(max_length=60)
